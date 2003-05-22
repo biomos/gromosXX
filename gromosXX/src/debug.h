a747ab003113104b094c4ccd7c851ca9e77120b5
Old Commits <chris.oostenbrink@boku.ac.at>
2003-05-22 08:40:54 +0000

    doxygen changes

--- gromosXX/src/debug.h
+++ gromosXX/src/debug.h
@@ -1,18 +1,20 @@
 /**
  * @file debug.h
  * define debug macros.
+ */
+ /**
  * @page debug Debug Levels
- *  -# : no output
- *  -# : important program decisions
- *  -# : important program path
- *  -# : 
- *  -# : program path (not unimportant functions)
- *  -# : function outline
- *  -# :
- *  -# : function detail
- *  -# :
- *  -# :
- *  -# : everything
+ *  -  0: no output
+ *  -  1: important program decisions
+ *  -  2: important program path
+ *  -  3: 
+ *  -  4: program path (not unimportant functions)
+ *  -  5: function outline
+ *  -  6:
+ *  -  7: function detail
+ *  -  8:
+ *  -  9:
+ *  - 10 : everything
  *
  * every file that uses it has to first
  * #define MODULE to the namespace name
