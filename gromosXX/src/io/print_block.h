1ecb8e779f6e9f47fc1258158b6d40adb03c9241
Old Commits <chris.oostenbrink@boku.ac.at>
2003-06-11 13:07:47 +0000

    release 0.0.2

--- gromosXX/src/io/print_block.h
+++ gromosXX/src/io/print_block.h
@@ -3,12 +3,6 @@
  * routines to print out the various blocks.
  */
 
-/**
- * Print the multibath block.
- */
-std::ostream & operator<<(std::ostream &os, simulation::Multibath &bath);
-
-
 namespace io
 {
   /** 
