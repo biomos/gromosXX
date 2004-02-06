f17d8ed1ecee9d622c29de03ada79ea172cc02a6
Old Commits <chris.oostenbrink@boku.ac.at>
2004-02-06 13:14:23 +0000

    adding needed files for auto-split up --markus

---
+++ gromosXX/src/interaction/nonbonded/create_header.h
@@ -0,0 +1,38 @@
+/**
+ * @file create_header.h
+ * include file for the automated split up nonbonded 
+ * creation process
+ */
+
+#include <util/stdheader.h>
+
+#include <topology/core/core.h>
+
+#include <topology/solute.h>
+#include <topology/solvent.h>
+#include <topology/perturbed_atom.h>
+#include <topology/perturbed_solute.h>
+
+#include <topology/topology.h>
+#include <simulation/multibath.h>
+#include <simulation/parameter.h>
+#include <simulation/simulation.h>
+#include <configuration/energy.h>
+#include <configuration/energy_average.h>
+#include <configuration/configuration.h>
+#include <algorithm/algorithm.h>
+#include <interaction/interaction.h>
+#include <interaction/forcefield/forcefield.h>
+
+#include <interaction/interaction_types.h>
+#include <math/periodicity.h>
+
+#include <io/instream.h>
+#include <io/topology/in_topology.h>
+
+// general
+#include <interaction/interaction.h>
+#include <interaction/forcefield/forcefield.h>
+
+#include "create_nonbonded.h"
+
