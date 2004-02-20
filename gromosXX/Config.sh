8b402a514f29bb2063faa45b00b5dcccfa4c7d43
Old Commits <chris.oostenbrink@boku.ac.at>
2004-02-20 14:11:36 +0000

    checking for nonbonded split up, added install instructions --markus

--- gromosXX/Config.sh
+++ gromosXX/Config.sh
@@ -3,6 +3,16 @@
 echo "preparing local settings"
 echo ""
 
+if [ -f src/interaction/nonbonded/split/Makefile.inc ] ; then
+	echo "nonbonded split up ok"
+	echo ""
+else
+	echo "nonbonded split up not ok"
+	echo "see INSTALL about nonbonded split-up"
+	echo ""
+	exit
+fi
+
 mkdir -p config
 aclocal &&
 libtoolize --copy &&
