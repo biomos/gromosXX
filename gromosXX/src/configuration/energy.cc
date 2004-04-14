aa76e81784ab21afdc958bfddf3887e17297cfe2
Markus Christen <m.christen@fake.com>
2004-04-14 11:43:39 +0000

    isnan typo

--- gromosXX/src/configuration/energy.cc
+++ gromosXX/src/configuration/energy.cc
@@ -144,7 +144,7 @@ int configuration::Energy::calculate_totals()
   total = potential_total + kinetic_total + special_total;
 
 #ifdef HAVE_ISNAN
-  if (isnan()){
+  if (isnan(total)){
     io::messages.add("total energy is NaN", "energy", io::message::error);
     return E_NAN;
   }
