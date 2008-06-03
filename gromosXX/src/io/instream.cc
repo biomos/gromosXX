21ff6d6de998d732f603d8ac04b2b1de126ae82e
Nathan Schmid <an.schmid@fake.com>
2008-06-03 12:56:24 +0000

    Handling of empty blocks. Fix of #81

--- gromosXX/src/io/instream.cc
+++ gromosXX/src/io/instream.cc
@@ -4,9 +4,11 @@
  */
 
 #include <stdheader.h>
+#include <vector>
 
 #include "blockinput.h"
 #include "instream.h"
+#include "message.h"
 
 #undef MODULE
 #undef SUBMODULE
@@ -25,29 +27,31 @@ void io::GInStream::readTitle() {
   title = io::concatenate(_b.begin() + 1, _b.end() - 1, title);
 }
 
-void io::GInStream::readStream()
-{
+void io::GInStream::readStream() {
   std::vector<std::string> buffer;
-  
-  while(!stream().eof()){
+  while (!stream().eof()) {
 
-    if (!io::getblock(stream(), buffer)){
-      if (buffer.size() && buffer[0] != ""){
-	std::cerr << "invalid block " + buffer[0] << " in input file?"
-		  << std::endl;
+    if (!io::getblock(stream(), buffer)) {
+      if (buffer.size() && buffer[0] != "") {
+        std::cerr << "invalid block " + buffer[0] << " in input file?"
+                << std::endl;
       }
       break;
     }
 
     trimblock(buffer);
-    
-    std::string n = buffer[0];
-    DEBUG(10, "reading block -" << buffer[0] << "- size " << buffer.size());
-    m_block[buffer[0]] = buffer;
-    
-    buffer.clear();
 
+    // empty blocks may cause problems
+    if (buffer.size() == 2) {
+      std::ostringstream out;
+      out << "empty block (" << buffer[0] << ")";
+      io::messages.add("GInStream", out.str(), io::message::error);
+    } else {
+      std::string n = buffer[0];
+      DEBUG(10, "reading block -" << buffer[0] << "- size " << buffer.size());
+      m_block[buffer[0]] = buffer;
+    }
+    buffer.clear();
   }
-  
 }
 
