c8d1efdcb28bc100707ee080583050ed17f97a37
Nathan Schmid <an.schmid@fake.com>
2008-08-06 11:54:31 +0000

    compilation fix for gcc 4.2

--- gromosXX/src/configuration/mesh.cc
+++ gromosXX/src/configuration/mesh.cc
@@ -27,8 +27,9 @@ configuration::GenericMesh<complex_type>::GenericMesh() : m_x(0), m_y(0), m_z(0)
 
 // this function is only used for complex numbers which has to be aligned
 // for FFTW
+namespace configuration {
 template<>
-configuration::GenericMesh<configuration::complex_number>::GenericMesh(unsigned int x, unsigned int y, unsigned int z) :
+GenericMesh<complex_number>::GenericMesh(unsigned int x, unsigned int y, unsigned int z) :
 m_x(x), m_y(y), m_z(z), m_volume(x*y*z) {
   // allocate the grid arrays. Here we have to use a dynamic array
   // because static arrays would lie on the stack and are not aligned
@@ -37,7 +38,7 @@ m_x(x), m_y(y), m_z(z), m_volume(x*y*z) {
   // statarr[x][y][z] == dynarr[z + Nz * (y + Ny * x)]
   // for more details see 3.2 Multidimensional Array format in section
   
-  m_mesh = (configuration::complex_number*) fftw_malloc(m_volume * sizeof(fftw_complex));
+  m_mesh = (complex_number*) fftw_malloc(m_volume * sizeof(fftw_complex));
   plan_forward = fftw_plan_dft_3d(m_x, m_y, m_z,
               reinterpret_cast<fftw_complex*> (m_mesh), reinterpret_cast<fftw_complex*> (m_mesh),
               FFTW_FORWARD, FFTW_ESTIMATE);
@@ -48,14 +49,14 @@ m_x(x), m_y(y), m_z(z), m_volume(x*y*z) {
 
 // this function is used for all other types.
 template<typename complex_type>
-configuration::GenericMesh<complex_type>::GenericMesh(unsigned int x, unsigned int y, unsigned int z) :
+GenericMesh<complex_type>::GenericMesh(unsigned int x, unsigned int y, unsigned int z) :
 m_x(x), m_y(y), m_z(z), m_volume(x*y*z) {
   m_mesh = new complex_type[m_volume];
 }
 
 // for complex number we also have to destroy the plans
 template<>
-configuration::GenericMesh<configuration::complex_number>::~GenericMesh() {
+GenericMesh<complex_number>::~GenericMesh() {
   // destory the plans
   if (plan_forward != NULL)
     fftw_destroy_plan(plan_forward);
@@ -70,13 +71,13 @@ configuration::GenericMesh<configuration::complex_number>::~GenericMesh() {
 
 // this function is used for all other types.
 template<typename complex_type>
-configuration::GenericMesh<complex_type>::~GenericMesh() {
+GenericMesh<complex_type>::~GenericMesh() {
   if (m_mesh != NULL)
     delete m_mesh;
 }
 
 template<>
-configuration::GenericMesh<configuration::complex_number>::GenericMesh(const GenericMesh<configuration::complex_number> & mesh) {
+GenericMesh<complex_number>::GenericMesh(const GenericMesh<complex_number> & mesh) {
   m_x = mesh.x();
   m_y = mesh.y();
   m_z = mesh.z();
@@ -88,7 +89,7 @@ configuration::GenericMesh<configuration::complex_number>::GenericMesh(const Gen
 }
 
 template<typename complex_type>
-configuration::GenericMesh<complex_type>::GenericMesh(const GenericMesh<complex_type> & mesh) {
+GenericMesh<complex_type>::GenericMesh(const GenericMesh<complex_type> & mesh) {
   m_x = mesh.x();
   m_y = mesh.y();
   m_z = mesh.z();
@@ -102,7 +103,7 @@ configuration::GenericMesh<complex_type>::GenericMesh(const GenericMesh<complex_
 
 // for the complex number we have to align
 template<>
-void configuration::GenericMesh<configuration::complex_number>::resize(unsigned int x, unsigned int y, unsigned int z){
+void GenericMesh<complex_number>::resize(unsigned int x, unsigned int y, unsigned int z){
   m_x = x;
   m_y = y;
   m_z = z;
@@ -116,7 +117,7 @@ void configuration::GenericMesh<configuration::complex_number>::resize(unsigned 
   if (m_mesh != NULL)
     fftw_free(m_mesh);
   
-  m_mesh = (configuration::complex_number*) fftw_malloc(m_volume * sizeof(fftw_complex));
+  m_mesh = (complex_number*) fftw_malloc(m_volume * sizeof(fftw_complex));
   plan_forward = fftw_plan_dft_3d(m_x, m_y, m_z,
               reinterpret_cast<fftw_complex*> (m_mesh), reinterpret_cast<fftw_complex*> (m_mesh),
               FFTW_FORWARD, FFTW_ESTIMATE);
@@ -126,7 +127,7 @@ void configuration::GenericMesh<configuration::complex_number>::resize(unsigned 
 }
 
 template<typename complex_type>
-void configuration::GenericMesh<complex_type>::resize(unsigned int x, unsigned int y, unsigned int z){
+void GenericMesh<complex_type>::resize(unsigned int x, unsigned int y, unsigned int z){
   m_x = x;
   m_y = y;
   m_z = z;
@@ -140,12 +141,12 @@ void configuration::GenericMesh<complex_type>::resize(unsigned int x, unsigned i
 
 // this function is only for the complex number
 template<>
-void configuration::GenericMesh<configuration::complex_number>::fft(configuration::GenericMesh<configuration::complex_number>::fft_type type) {
+void GenericMesh<complex_number>::fft(GenericMesh<complex_number>::fft_type type) {
   switch (type) {
-    case configuration::GenericMesh<configuration::complex_number>::fft_forward :
+    case GenericMesh<complex_number>::fft_forward :
               fftw_execute(plan_forward);
       break;
-    case configuration::GenericMesh<configuration::complex_number>::fft_backward :
+    case GenericMesh<complex_number>::fft_backward :
               fftw_execute(plan_backward);
       break;
     default:
@@ -155,8 +156,9 @@ void configuration::GenericMesh<configuration::complex_number>::fft(configuratio
 }
 
 template<typename complex_type>
-void configuration::GenericMesh<complex_type>::fft(configuration::GenericMesh<complex_type>::fft_type type) {
+void GenericMesh<complex_type>::fft(GenericMesh<complex_type>::fft_type type) {
   throw std::runtime_error("FFT of a complex data type not implemented");
+}
 }
 
 // make sure you instantiate the template with all types you need here.
