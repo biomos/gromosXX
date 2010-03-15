07e3a04e8e476776cc73a65bee5bb613a5844452
Sereina Riniker <sriniker@ethz.ch>
2010-03-15 10:01:25 +0000

    better numerical implementation of EDS reference state (#195)

--- gromosXX/src/interaction/forcefield/forcefield.cc
+++ gromosXX/src/interaction/forcefield/forcefield.cc
@@ -115,9 +115,12 @@ int interaction::Forcefield
   for (iterator it = begin(), to = end();
       it != to;
       ++it){
-    DEBUG(7, "interaction: " << (*it)->name);
+    DEBUG(5, "interaction: " << (*it)->name);
     if ((*it)->calculate_interactions(topo, conf, sim))
       return 1;
+    DEBUG(5, "force old = " << math::v2s(conf.old().force(2)));
+    DEBUG(5, "force current = " << math::v2s(conf.current().force(2)));
+    DEBUG(5, "force special = " << math::v2s(conf.special().eds.force_endstates[0](2)));
   }
 
   if (sim.param().eds.eds) {
@@ -125,36 +128,45 @@ int interaction::Forcefield
     switch (sim.param().eds.form) {
       case simulation::single_s : {
         // interactions have been calculated - now apply eds Hamiltonian
-        std::vector<long double> prefactors(numstates);
+        std::vector<double> prefactors(numstates);
         // get beta
         assert(sim.param().multibath.multibath.bath(0).temperature != 0.0);
         const double beta = 1.0 / (sim.param().multibath.multibath.bath(0).temperature * math::k_Boltzmann);
         assert(sim.param().eds.s.size()==1);
         const double s = sim.param().eds.s[0];
-        long double sum_prefactors = 0.0;
         const std::vector<double> & eds_vi = conf.current().energies.eds_vi;
         const std::vector<double> & eir = sim.param().eds.eir;
+        unsigned int state_i = 0;
+        unsigned int state_j = 1;
+        double partA = -beta * s * (eds_vi[state_i] - eir[state_i]);
+        double partB = -beta * s * (eds_vi[state_j] - eir[state_j]);
+        double sum_prefactors = std::max(partA, partB)
+                + log(1 + exp(std::min(partA, partB) - std::max(partA, partB)));
+        prefactors[state_i] = partA;
+        prefactors[state_j] = partB;
 
-        for (unsigned int state = 0; state < numstates; state++) {
-          const double pre = exp(-beta * s * (eds_vi[state] - eir[state]));
-          prefactors[state] = pre;
-          sum_prefactors += pre;
+        for (unsigned int state = 2; state < numstates; state++) {
+          double part = -beta * s * (eds_vi[state] - eir[state]);
+          sum_prefactors = std::max(sum_prefactors, part)
+                    + log(1 + exp(std::min(sum_prefactors, part) - std::max(sum_prefactors, part)));
+          prefactors[state] = part;
           DEBUG(7, "eds_vi[ " << state << "] = " << eds_vi[state]);
           DEBUG(7, "eir[" << state << "]" << eir[state]);
-          DEBUG(7, "pre = " << pre);
+          DEBUG(7, "part = " << part);
         }
         // calculate eds Hamiltonian
-        conf.current().energies.eds_vr = -1.0 / (beta * s) * log(sum_prefactors);
+        conf.current().energies.eds_vr = -1.0 / (beta * s) * sum_prefactors;
         DEBUG(7, "eds_vr = " << conf.current().energies.eds_vr);
-        // calculate eds contribution ...
-        const double sum_prefactors_i = 1.0 / sum_prefactors;
 
+        // calculate eds contribution ...
         for (unsigned int state = 0; state < numstates; state++) {
-          const long double pi = prefactors[state] * sum_prefactors_i;
+          const long double pi = exp(prefactors[state] - sum_prefactors);
           //std::cerr << "state = " << state << ", pi = " << pi << std::endl;
           // ... to forces
+          DEBUG(7, "prefactor = " << pi);
           for (unsigned int i = 0; i < topo.num_atoms(); i++) {
             conf.current().force(i) += pi * conf.special().eds.force_endstates[state](i);
+            DEBUG(9, "force current: " << i << " = " << math::v2s(conf.current().force(i)));
           }
           // ... to virial
           for (int a = 0; a < 3; ++a) {
@@ -169,7 +181,7 @@ int interaction::Forcefield
       }
       case simulation::multi_s : {
         // interactions have been calculated - now apply eds Hamiltonian
-        std::vector<long double> prefactors(numstates,0.0);
+        std::vector<double> prefactors(numstates,0.0);
         // get beta
         assert(sim.param().multibath.multibath.bath(0).temperature != 0.0);
         const double beta = 1.0 / (sim.param().multibath.multibath.bath(0).temperature * math::k_Boltzmann);
@@ -180,40 +192,66 @@ int interaction::Forcefield
         const std::vector<double> & eds_vi = conf.current().energies.eds_vi;
         const std::vector<double> & eir = sim.param().eds.eir;
         const std::vector<double> & s = sim.param().eds.s;
-        long double sum_prefactors = 0.0;
-        for (unsigned int state_i = 0; state_i < (numstates - 1); state_i++){
-          for (unsigned int state_j = state_i + 1; state_j < numstates; state_j++){
-            const unsigned int pair_index = (state_i*(2 * numstates - state_i-1)) / 2 + state_j - state_i - 1;
+        // first pair
+        unsigned int state_i = 0;
+        unsigned int state_j = 1;
+        unsigned int pair_index = 0;
+        double partA = -beta * s[pair_index] * (eds_vi[state_i] - eir[state_i]);
+        double partB = -beta * s[pair_index] * (eds_vi[state_j] - eir[state_j]);
+        double sum_prefactors = (std::max(partA, partB)
+                + log(1 + exp(std::min(partA, partB) - std::max(partA, partB))))
+                / s[pair_index];
+        double elem2 = (std::max(partA, partB)
+                  + log(1 + exp(std::min(partA, partB) - std::max(partA, partB))))
+                  * (1/s[pair_index] -1);
+        double pre_i_force = partA + elem2;
+        double pre_j_force = partB + elem2;
+        prefactors[state_i] = std::max(prefactors[state_i], pre_i_force)
+                  + log(1 + exp(std::min(prefactors[state_i], pre_i_force)));
+        prefactors[state_j] = std::max(prefactors[state_j], pre_j_force)
+                  + log(1 + exp(std::min(prefactors[state_j], pre_j_force)
+                  - std::max(prefactors[state_j], pre_j_force)));
+        DEBUG(7, "pre_i_force = " << pre_i_force << ", pre_j_force " << pre_j_force);
+        DEBUG(7, "s_" << state_i << state_j << " = " << s[pair_index]);
+        for (state_i = 1; state_i < (numstates - 1); state_i++){
+          for (state_j = state_i + 1; state_j < numstates; state_j++) {
+            pair_index = (state_i * (2 * numstates - state_i - 1)) / 2 + state_j - state_i - 1;
             DEBUG(7, "index of pair " << state_i << " - " << state_j << " = " << pair_index);
             // get the correct s_ij value
-            // Hamiltonian
             const double s_ij = s[pair_index];
             assert(s_ij > 0);
-            const double pre_i = exp(-beta * s_ij * (eds_vi[state_i] - eir[state_i]));
-            const double pre_j = exp(-beta * s_ij * (eds_vi[state_j] - eir[state_j]));
-            const double pre_i_pre_j = pre_i + pre_j;
-            const double pipj_pow1 = pow(pre_i_pre_j,1.0/s_ij);
-            const double pipj_pow2 = pow(pre_i_pre_j,1.0/s_ij - 1);
-            sum_prefactors+=pipj_pow1;
+            partA = -beta * s_ij * (eds_vi[state_i] - eir[state_i]);
+            partB = -beta * s_ij * (eds_vi[state_j] - eir[state_j]);
+            double elem = (std::max(partA, partB)
+                    + log(1 + exp(std::min(partA, partB) - std::max(partA, partB))))
+                    / s_ij;
+            elem2 = (std::max(partA, partB)
+                    + log(1 + exp(std::min(partA, partB) - std::max(partA, partB))))
+                    * (1 / s_ij - 1);
+            sum_prefactors = std::max(sum_prefactors, elem)
+                    + log(1 + exp(std::min(sum_prefactors, elem) - std::max(sum_prefactors, elem)));
+            DEBUG(7, "partA = " << partA << " , partB = " << partB << " , elem = "
+                    << elem << " , sum = " << sum_prefactors);
             // additional calculation for forces
-            const double pre_i_force = pipj_pow2*pre_i;
-            const double pre_j_force = pipj_pow2*pre_j;
-            prefactors[state_i] += pre_i_force;
-            prefactors[state_j] += pre_j_force;
-            DEBUG(7, "eds prefactor pair " << state_i << " - " << state_j << ", pre_i = " << pre_i << ", pre_j " << pre_j);
-            DEBUG(7, "eds prefactor pair " << state_i << " - " << state_j << ", Vr component = " << pipj_pow1);
-            DEBUG(7, "s_" << state_i << state_j << " = " << s_ij );
-           
+            pre_i_force = partA + elem2;
+            pre_j_force = partB + elem2;
+            prefactors[state_i] = std::max(prefactors[state_i], pre_i_force)
+                    + log(1 + exp(std::min(prefactors[state_i], pre_i_force)
+                    - std::max(prefactors[state_i], pre_i_force)));
+            prefactors[state_j] = std::max(prefactors[state_j], pre_j_force)
+                    + log(1 + exp(std::min(prefactors[state_j], pre_j_force)
+                    - std::max(prefactors[state_j], pre_j_force)));
+            DEBUG(7, "pre_i_force = " << pre_i_force << ", pre_j_force " << pre_j_force);
+            DEBUG(7, "s_" << state_i << state_j << " = " << s_ij);
           }
         }
         // calculate eds Hamiltonian
-        conf.current().energies.eds_vr = -1.0 / beta * log(sum_prefactors/(numstates-1));
+        conf.current().energies.eds_vr = -1.0 / beta * (sum_prefactors + log(1.0 / double(numstates - 1)));
         DEBUG(7, "eds_vr = " << conf.current().energies.eds_vr);
-        // calculate eds contribution ...
-        const double sum_prefactors_i = 1.0 / sum_prefactors;
 
+        // calculate eds contribution ...
         for (unsigned int state = 0; state < numstates; state++) {
-          const long double pi = prefactors[state] * sum_prefactors_i;
+          const long double pi = exp(prefactors[state] - sum_prefactors);
           //std::cerr << "state = " << state << ", pi = " << pi << std::endl;
           // ... to forces
           for (unsigned int i = 0; i < topo.num_atoms(); i++) {
@@ -232,7 +270,7 @@ int interaction::Forcefield
       }
       case simulation::pair_s : {
         // interactions have been calculated - now apply eds Hamiltonian
-        std::vector<long double> prefactors(numstates, 0.0);
+        std::vector<double> prefactors(numstates, 0.0);
         // get beta
         assert(sim.param().multibath.multibath.bath(0).temperature != 0.0);
         const double beta = 1.0 / (sim.param().multibath.multibath.bath(0).temperature * math::k_Boltzmann);
@@ -241,9 +279,28 @@ int interaction::Forcefield
         const std::vector<double> & eds_vi = conf.current().energies.eds_vi;
         const std::vector<double> & eir = sim.param().eds.eir;
         const std::vector<double> & s = sim.param().eds.s;
-        long double sum_prefactors = 0.0;
+        unsigned int state_i = sim.param().eds.pairs[0].i - 1;
+        unsigned int state_j = sim.param().eds.pairs[0].j - 1;
+        double partA = -beta * s[0] * (eds_vi[state_i] - eir[state_i]);
+        double partB = -beta * s[0] * (eds_vi[state_j] - eir[state_j]);
+        double sum_prefactors = (std::max(partA, partB)
+                + log(1 + exp(std::min(partA, partB) - std::max(partA, partB))))
+                / s[0];
+        double elem2 = (std::max(partA, partB)
+                  + log(1 + exp(std::min(partA, partB) - std::max(partA, partB))))
+                  * (1/s[0] -1);
+        double pre_i_force = partA + elem2;
+        double pre_j_force = partB + elem2;
+        prefactors[state_i] = std::max(prefactors[state_i], pre_i_force)
+                  + log(1 + exp(std::min(prefactors[state_i], pre_i_force)));
+        prefactors[state_j] = std::max(prefactors[state_j], pre_j_force)
+                  + log(1 + exp(std::min(prefactors[state_j], pre_j_force)
+                  - std::max(prefactors[state_j], pre_j_force)));
+        DEBUG(7, "pre_i_force = " << pre_i_force << ", pre_j_force " << pre_j_force);
+        DEBUG(7, "s_" << state_i << state_j << " = " << s[0]);
+
         // loop over (N-1) EDS state pairs
-        for (unsigned int pair = 0; pair < sim.param().eds.pairs.size(); pair++) {
+        for (unsigned int pair = 1; pair < sim.param().eds.pairs.size(); pair++) {
           assert(pair < s.size());
           const double s_ij = s[pair];
           assert(s_ij > 0);
@@ -251,32 +308,43 @@ int interaction::Forcefield
           unsigned int state_j = sim.param().eds.pairs[pair].j - 1;
           assert(state_i < eir.size() && state_i < eds_vi.size());
           assert(state_j < eir.size() && state_j < eds_vi.size());
-          const double pre_i = exp(-beta * s_ij * (eds_vi[state_i] - eir[state_i]));
-          const double pre_j = exp(-beta * s_ij * (eds_vi[state_j] - eir[state_j]));
-          const double pre_i_pre_j = pre_i + pre_j;
-          const double pipj_pow1 = pow(pre_i_pre_j, 1.0 / s_ij);
-          const double pipj_pow2 = pow(pre_i_pre_j, 1.0 / s_ij - 1);
-          sum_prefactors += pipj_pow1;
+          partA = -beta * s_ij * (eds_vi[state_i] - eir[state_i]);
+          partB = -beta * s_ij * (eds_vi[state_j] - eir[state_j]);
+          double elem = (std::max(partA, partB)
+                  + log(1 + exp(std::min(partA, partB) - std::max(partA, partB))))
+                  / s_ij;
+          elem2 = (std::max(partA, partB)
+                  + log(1 + exp(std::min(partA, partB) - std::max(partA, partB))))
+                  * (1/s_ij -1);
+          sum_prefactors = std::max(sum_prefactors, elem)
+                  + log(1 + exp(std::min(sum_prefactors, elem) - std::max(sum_prefactors, elem)));
+          DEBUG(7, "partA = " << partA << " , partB = " << partB << " , elem = "
+                  << elem << " , sum = " << sum_prefactors);
           // additional calculation for forces
-          const double pre_i_force = pipj_pow2*pre_i;
-          const double pre_j_force = pipj_pow2*pre_j;
-          prefactors[state_i] += pre_i_force;
-          prefactors[state_j] += pre_j_force;
-          DEBUG(7, "eds prefactor pair " << state_i << " - " << state_j << ", pre_i = " << pre_i << ", pre_j " << pre_j);
-          DEBUG(7, "eds prefactor pair " << state_i << " - " << state_j << ", Vr component = " << pipj_pow1);
+          pre_i_force = partA + elem2;
+          pre_j_force = partB + elem2;
+          prefactors[state_i] = std::max(prefactors[state_i], pre_i_force)
+                  + log(1 + exp(std::min(prefactors[state_i], pre_i_force)
+                  - std::max(prefactors[state_i], pre_i_force)));
+          prefactors[state_j] = std::max(prefactors[state_j], pre_j_force)
+                  + log(1 + exp(std::min(prefactors[state_j], pre_j_force)
+                  - std::max(prefactors[state_j], pre_j_force)));
+          DEBUG(7, "pre_i_force = " << pre_i_force << ", pre_j_force " << pre_j_force);
           DEBUG(7, "s_" << state_i << state_j << " = " << s_ij);
         }
         // calculate eds Hamiltonian
-        conf.current().energies.eds_vr = -1.0 / beta * log(sum_prefactors * numstates / ((numstates - 1)*2));
-        DEBUG(7, "eds_vr = " << conf.current().energies.eds_vr);
-        // calculate eds contribution ...
-        const double sum_prefactors_i = 1.0 / sum_prefactors;
+        DEBUG(7, "sum = " << sum_prefactors << " , num_states = " << numstates);
+        conf.current().energies.eds_vr = -1.0 / beta * (sum_prefactors + log(double(numstates) / double((numstates - 1)*2)));
+        DEBUG(3, "eds_vr = " << conf.current().energies.eds_vr);
 
+        // calculate eds contribution ...
         for (unsigned int state = 0; state < numstates; state++) {
-          const long double pi = prefactors[state] * sum_prefactors_i;
+          const long double pi = exp(prefactors[state] - sum_prefactors);
+          DEBUG(3, "pi = " << pi << " , prefactors = " << prefactors[state]);
           // ... to forces
           for (unsigned int i = 0; i < topo.num_atoms(); i++) {
             conf.current().force(i) += pi * conf.special().eds.force_endstates[state](i);
+            DEBUG(7, "force = " << math::v2s(conf.special().eds.force_endstates[state](i)));
           }
           // ... to virial
           for (int a = 0; a < 3; ++a) {
