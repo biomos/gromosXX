09a5051133594ff20146daf4e3d6f291448937cf
Niels Hansen <hansen@itt.uni-stuttgart.de>
2011-12-16 15:22:22 +0000

    Unlocked steepest descent for CG

--- gromosXX/src/io/parameter/check_parameter.cc
+++ gromosXX/src/io/parameter/check_parameter.cc
@@ -415,7 +415,7 @@ int io::check_parameter(simulation::Simulation & sim)
   fc.unlock("steepest_descent", "cutoff_atomic");
   fc.unlock("steepest_descent", "cg_martini");
   fc.unlock("steepest_descent", "multi_grain");
-//  fc.unlock("steepest_descent", "cg_gromos");
+  fc.unlock("steepest_descent", "cg_gromos");
 //  fc.unlock("steepest_descent", "mixed_grain");
   fc.unlock("steepest_descent", "temp_berendsen");
   fc.unlock("steepest_descent", "temp_nosehoover");
