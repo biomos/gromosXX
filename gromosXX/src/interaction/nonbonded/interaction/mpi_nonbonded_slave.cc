7a85a9732a20730af492307a6fcc23d26a5f14c1
Katharina Meier <k.meier@fake.com>
2012-01-17 08:37:45 +0000

    bug fix mpi for QM/MM

--- gromosXX/src/interaction/nonbonded/interaction/mpi_nonbonded_slave.cc
+++ gromosXX/src/interaction/nonbonded/interaction/mpi_nonbonded_slave.cc
@@ -104,7 +104,14 @@ int interaction::MPI_Nonbonded_Slave::calculate_interactions
 			  MPI::DOUBLE,
 			  0);
     
-    // std::cerr << "slave: receiving box" << std::endl;
+    // bcast charges (for QM/MM)
+	            MPI::COMM_WORLD.Bcast(&topo.charge()[0],
+			                        topo.num_atoms(),
+			                       MPI::DOUBLE,
+			          0);
+
+
+	// std::cerr << "slave: receiving box" << std::endl;
     MPI::COMM_WORLD.Bcast(&conf.current().box(0)(0),
 	                  9,
                           MPI::DOUBLE,
