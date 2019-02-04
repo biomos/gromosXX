ea35ff98b6b4a892f3262258b2c7c08adf4b6a4e
schroederB <benjamin-schroeder@outlook.com>
2019-02-04 15:18:58 +0100

    added reeds files

---
+++ gromosXX/src/util/replicaExchange/replica_exchange_base_eds.h
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   replica_exchange_base_eds.h
+ * Author: bschroed
+ *
+ * Created on April 18, 2018, 3:38 PM
+ */
+#include <util/replicaExchange/replica_exchange_base.h>
+#include <util/replicaExchange/replica_data.h>
+#include <util/replicaExchange/repex_mpi.h>
+#include <util/replicaExchange/replica_reeds.h>
+
+#ifndef REPLICA_EXCHANGE_BASE_EDS_H
+#define REPLICA_EXCHANGE_BASE_EDS_H
+
+//const
+
+#include <stdheader.h>
+
+#include <algorithm/algorithm.h>
+#include <topology/topology.h>
+#include <simulation/simulation.h>
+#include <configuration/configuration.h>
+
+#include <algorithm/algorithm/algorithm_sequence.h>
+#include <interaction/interaction.h>
+#include <interaction/forcefield/forcefield.h>
+
+#include <io/argument.h>
+#include <util/usage.h>
+#include <util/error.h>
+
+#include <io/read_input.h>
+#include <io/print_block.h>
+
+#include <time.h>
+#include <unistd.h>
+
+#include <io/configuration/out_configuration.h>
+
+
+#include <string>
+#include <math/random.h>
+
+#ifdef XXMPI
+#include <mpi.h>
+#endif
+
+namespace util
+{
+     class replica_exchange_base_eds : public virtual replica_exchange_base {
+    public:
+        replica_exchange_base_eds(io::Argument _args, int cont, int rank, std::vector<int> repIDs, 
+                          std::map<ID_t, rank_t>& _repMap);
+         
+        /**
+         * @override
+         * Tries a swapping of configuration if possible. Calculates energies, probabilities
+         * and sends information via MPI communication if necessary. Added also reeds information
+         */
+        //using util::replica_exchange_base::swap;
+        void swap() override;
+        /**
+        * runs MD simulation for all replicas; one by one
+        */
+        void run_MD() override;
+        /**
+         * inits replica_reeds
+         */
+        void init() override;
+        
+         /** 
+         * calculate and write out acceptance probabilities for non-neighbour
+         * switching attempts. Useful to optimize replica EDS parameter choice. 
+         */
+        void eds_stat();
+          /** 
+         * Initialize file and data for eds_stat output.
+         */
+        void init_eds_stat();
+        
+    protected:
+        /**
+        * all replicas on this node
+        */
+        typedef std::vector< util::replica_reeds* >::iterator repIterator;   //iterator for replicas
+        std::vector<util::replica_reeds*> replicas;
+        
+        void switch_coords_on_node(repIterator it1, const unsigned int partner);
+        void swap_on_node(repIterator it1, const unsigned int partner);
+        virtual ~replica_exchange_base_eds();
+
+       /**
+       * stat information of all replicas for exchange optimisation 
+       */
+       std::map<ID_t, util::reeds_replica_stat_data > replicaStatData;
+       /**
+       *  output file stream for output file
+       */
+       std::map<ID_t, std::ofstream *> eds_stat_out;
+       
+        /**
+        *  Other Functions:
+        */
+        //init Replicas - used in contstructor, initialises the replica objs.
+        void createReplicas(int cont, std::vector<int>  repIDs, int rank) override;
+    };
+}
+#endif /* REPLICA_EXCHANGE_BASE_EDS_H */
+
---
+++ gromosXX/src/util/replicaExchange/replica_reeds.cc
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   replica_reeds.cc
+ * Author: bschroed
+ * 
+ * Created on April 23, 2018, 3:25 PM
+ */
+
+#include <util/replicaExchange/replica_reeds.h>
+
+
+#undef MODULE
+#undef SUBMODULE
+#define MODULE util
+#define SUBMODULE replica
+
+util::replica_reeds::replica_reeds(io::Argument _args, int cont, int _ID, int _rank): replica(_args, cont, _ID, _rank){
+  // read input again. If copy constructors for topo, conf, sim, md work, one could
+  // also pass them down from repex_mpi.cc ...  
+ DEBUG(3, "replica_reeds "<< rank <<":\t constructor \t START");
+ DEBUG(2, "replica_reeds "<< rank <<":\t constructor \t Run="<<run); //todo:remove bschroed!@
+  //TODO: override eventually more sim params! bschroed
+  eds_para=sim.param().reeds.eds_para[ID];
+  sim.param().eds = eds_para;
+  
+  const int num_s = sim.param().reeds.num_l;
+  l == eds_para.s[0];
+  DEBUG(4, "Temp of replica "<< rank <<" " << _ID << " \t" << sim.param().multibath.multibath.bath(0).temperature)
+  DEBUG(4, "S of replica "<< rank <<" " << _ID << " \t" << sim.param().eds.s[0])
+  DEBUG(4, "S size of replica "<< rank <<" " << _ID << " \t" << sim.param().eds.s.size())
+
+
+  assert(0.0 <= sim.param().multibath.multibath.bath(0).temperature);
+  DEBUG(3, "replica_reeds "<< rank <<":\t constructor \t DONE"); 
+
+}
+
+void util::replica_reeds::reset_eds() {//only reset switched parameters of change_eds() function 
+  sim.param().eds = eds_para;
+  sim.param().step.dt = dt;
+  conf.current().force=force_orig;
+  conf.current().virial_tensor=virial_tensor_orig;
+}
+
+void util::replica_reeds::change_eds(const unsigned int partner){//only change parameters, which are needed for energy calculation i.e. 
+  int idx;
+  if (sim.param().reeds.num_l == 1)
+    idx = 0;
+  else
+    idx = partner;
+  sim.param().step.dt = sim.param().reeds.dt[idx];
+  sim.param().eds=sim.param().reeds.eds_para[idx];
+  force_orig = conf.current().force;
+  virial_tensor_orig = conf.current().virial_tensor;
+  //further changes necessary!? TODO:bschroed
+}
+
+/*
+ * calc_energy_eds_stat() is only used for statistical purposes in eds_stat()
+ * In order to avoid any adjustment of the mpi communication and thus reducing the complexity, the 
+ * energy_calculation and probability calculations from replica.cc are not adjusted to work 
+ * for non-pairwise exchanges. Instead, calc_energy_eds_stat() calculates the potential energy
+ * of the current configuration for a new smoothing parameter s.
+ * The exchange probabilities can be calculated in a postprocessing step, using these energies
+ * given in the energy_stat output files.
+ */
+double util::replica_reeds::calc_energy_eds_stat(double s){
+    double old_dt;
+    double old_s;
+    double old_eds_vr;
+    algorithm::Algorithm * ff;   
+    if(sim.param().eds.eds){
+          //to reset old state
+          old_dt=sim.param().step.dt;
+          old_s=sim.param().eds.s[0];
+          old_eds_vr=conf.current().energies.eds_vr;
+          force_orig = conf.current().force;
+          virial_tensor_orig = conf.current().virial_tensor;
+          //only temporary change
+          sim.param().eds.s[0]=s;
+          
+          ff = md.algorithm("EDS");
+    }
+    else {
+          print_info("eds_stat() i.e calc_energy_eds_stat() called for non EDS simulation!");
+      #ifdef XXMPI
+          MPI_Abort(MPI_COMM_WORLD, E_UNSPECIFIED);
+      #endif
+    }
+    
+    //Calculate energies
+    if (ff->apply(topo, conf, sim)) {
+      print_info("Error in Forcefield energy calculation!");
+     #ifdef XXMPI
+      MPI_Abort(MPI_COMM_WORLD, E_UNSPECIFIED);
+     #endif
+      return 1;
+    }
+    
+    double energy=conf.current().energies.eds_vr; 
+    
+    // reset old EDS state
+    conf.current().energies.eds_vr=old_eds_vr;
+    sim.param().eds.s[0] = old_s;
+    sim.param().step.dt = old_dt;
+    conf.current().force=force_orig;
+    conf.current().virial_tensor=virial_tensor_orig;
+    
+    return energy;
+}
+
+double util::replica_reeds::calculate_energy(const int partner) {
+    DEBUG(2, "replica_reeds:\t calculate_energy \t START"); 
+
+    double energy = 0.0;
+    algorithm::Algorithm * ff;   
+
+    DEBUG(2, "replica_reeds:\t calculate_energy \t\t get Partner settings"); 
+    if(partner!=ID) change_eds(partner);
+    ff = md.algorithm("EDS");
+
+    //Calculate energies    
+    DEBUG(2, "replica_reeds:\t calculate_energy \t\t calc energies"); 
+    if (ff->apply(topo, conf, sim)) {
+      print_info("Error in Forcefield energy calculation!");
+  #ifdef XXMPI
+      MPI_Abort(MPI_COMM_WORLD, E_UNSPECIFIED);
+  #endif
+      return 1;
+    }
+    
+    //return energies
+    DEBUG(2, "replica_reeds:\t calculate_energy \t\t return energies"); 
+    energy=conf.current().energies.eds_vr; 
+    if(partner!=ID) reset_eds();
+    
+    DEBUG(2, "replica_reeds:\t calculate_energy \t DONE"); 
+    return energy;
+}
