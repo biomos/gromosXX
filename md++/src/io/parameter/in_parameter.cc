00680b46ef525d9739391f4c281163e5a66790ce
Maria Pechlaner <maria.pechlaner@boku.ac.at>
2021-06-08 18:15:43 +0200

    tfrdcres: scale input force constant by 1e-6

--- md++/src/io/parameter/in_parameter.cc
+++ md++/src/io/parameter/in_parameter.cc
@@ -2578,7 +2578,7 @@ void io::In_Parameter::read_TFRDCRES(simulation::Parameter &param,
     exampleblock << "#           0                start from initial values of D0 [default]\n";
     exampleblock << "#           1                read time averages from startup file\n";
     exampleblock << "#                            (for continuation time-averaged run)\n";
-    exampleblock << "# CTFRDC   >= 0.0            RDC restraining force constant [kJ*s^2/ mol]\n";
+    exampleblock << "# CTFRDC   >= 0.0            RDC restraining force constant [10^-6 kJ*s^2/ mol]\n";
     exampleblock << "#                            (weighted by individual WTFRDC)\n";
     exampleblock << "# TAUR     >= 0.0            r coupling time for time-averaging\n";
     exampleblock << "# TAUT     >= 0.0            theta coupling time for time-averaging\n";
@@ -2604,6 +2604,8 @@ void io::In_Parameter::read_TFRDCRES(simulation::Parameter &param,
         block.get_next_parameter("TAUR", param.tfrdc.taur, ">=0", "");
         block.get_next_parameter("TAUT", param.tfrdc.taut, ">=0", "");
         block.get_next_parameter("NTWTFRDC", param.tfrdc.write, ">=0", "");
+
+        param.tfrdc.K/=1000000;
 
 
         switch (nttfrdc) {
