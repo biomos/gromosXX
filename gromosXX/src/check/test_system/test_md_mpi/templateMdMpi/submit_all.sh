7a1b7e0e6f5e59fc760095804d6bd5db62be9bb7
schroederb <benjamin-ries@outlook.com>
2021-11-22 13:25:49 +0100

    nice md_mpi works

--- gromosXX/src/check/test_system/test_md_mpi/templateMdMpi/submit_all.sh
+++ gromosXX/src/check/test_system/test_md_mpi/templateMdMpi/submit_all.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/bash 
+
+# load settings
+. ../../gromos_settings.sh
+
+#how many cares to be tested on 
 cores_per_rep=(1 2 3 4 );
-#cores_per_rep=(8 10);
+
 jobs_prefix="md_mpi"
 gromosBIN=${1}
 
 for i in "${cores_per_rep[@]}"; 
 do 
-echo "$i using nodes: ${i}";
-tmp_prefix="${jobs_prefix}_N${i}" 
-rm -r ${tmp_prefix}
-cp -r template ${tmp_prefix}
-cd ${tmp_prefix}
-./job.sh ${tmp_prefix} ${i} ${gromosBIN}
-cd ..
+    tmp_prefix="${jobs_prefix}_N${i}" 
+    echo -e "\t Testing: $tmp_prefix using nodes: ${i}    ${gromosBIN}";
+
+
+    rm -rf ${tmp_prefix}
+    cp -r template ${tmp_prefix}
+
+    cd ${tmp_prefix}
+    ./job.sh ${tmp_prefix} ${i} ${gromosBIN} || echo "Failed: ${x} " >> ../Failed.log
+    cd ..
 
 done
