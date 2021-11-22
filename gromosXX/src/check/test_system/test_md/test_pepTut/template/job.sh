9f93effa5fbb14565f1754f2683c7af3f1bb5c5d
schroederb <benjamin-ries@outlook.com>
2021-11-22 13:55:19 +0100

    restructuring for sub examples

---
+++ gromosXX/src/check/test_system/test_md/test_pepTut/template/job.sh
@@ -0,0 +1,31 @@
+#Load Gromos Settings
+. ../../gromos_settings.sh
+
+#remove previous files
+rm -f test*;
+gromDIR=${1} 
+
+if [ ${gromDIR} == *debug* ]
+then
+    ${gromDIR}/md \
+        @topo ./input/peptide_2Cl_54a7.top\
+        @conf ./input/eq_peptide_5.cnf\
+        @input ./input/md.imd \
+        @fin test.cnf \
+        @trc test.trc \
+        @tre test.tre \
+        @trs test.trs \
+        @verb 5 \
+        ${1} >test.omd || exit 1
+else
+
+    ${gromDIR}/md \
+        @topo ./input/peptide_2Cl_54a7.top\
+        @conf ./input/eq_peptide_5.cnf\
+        @input ./input/md.imd \
+        @fin test.cnf \
+        @trc test.trc \
+        @tre test.tre \
+        @trs test.trs \
+        ${1} >test.omd || exit 1
+fi
---
+++ gromosXX/src/check/test_system/test_repex_mpi/test_RE_EDS_1D/templateReeds/submit_all.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash 
+
+#Load Gromos Settings
+. ../../gromos_settings.sh
+
+cores_per_rep=(1); #2 
+replicas=2
+
+jobs_prefix="1DRepEx_${replicas}"
+gromosBIN=${1}
+
+for i in "${cores_per_rep[@]}"; 
+do 
+    echo "$i using nodes: $((i*replicas))";
+    tmp_prefix="${jobs_prefix}_N${i}" 
+    rm -rf ${tmp_prefix}
+    cp -r template ${tmp_prefix}
+
+    cd ${tmp_prefix}
+    ./job.sh ${tmp_prefix} $((i*2)) ${gromosBIN}  || echo "Failed: ${x} " >> ../Failed.log
+    cd ..
+
+done
+
