f3cc7339256a1ce636aba5281226a91abce05e63
Felix Pultar <felix.pultar@phys.chem.ethz.ch>
2022-04-23 17:00:12 +0200

    Started to implement a test system
    Signed-off-by: Felix Pultar <felix.pultar@phys.chem.ethz.ch>

--- gromosXX/src/check/orca_worker.t.cc
+++ gromosXX/src/check/orca_worker.t.cc
@@ -3,7 +3,18 @@
 #include "check.h"
 
 #include "qm_worker.t.h"
+#include "orca_worker.t.h"
 
+test::Orca_Worker_Test::Orca_Worker_Test(std::string binary_name
+               , std::string test_title
+               , std::string topology_file
+               , std::string simulation_file
+               , std::string configuration_file
+               , std::string qmmm_file
+               , std::string coordinate_trajectory_file
+               , std::string energy_trajectory_file
+               , std::string final_configuration_file)
+               : test::QM_Worker_Test::QM_Worker_Test(binary_name, test_title, topology_file, simulation_file, configuration_file, qmmm_file, coordinate_trajectory_file, energy_trajectory_file, final_configuration_file) {}
 
 int main(int argc, char** argv) {
   // set up simulation files
@@ -18,6 +29,6 @@ int main(int argc, char** argv) {
   GETFILEPATH(fin_f, "md_final.cnf", ROOT_FOLDER);
   
   // initialize test
-  test::QM_Worker_Test qm_worker_test(argv[0], "QM/MM Test", topo_f, input_f, conf_f, qmmm_f, trc_f, tre_f, fin_f);
-  qm_worker_test.test_friend();
+  test::QM_Worker_Test orca_worker_test(argv[0], "Orca QM/MM Test", topo_f, input_f, conf_f, qmmm_f, trc_f, tre_f, fin_f);
+  orca_worker_test.test_friend();
 }
--- gromosXX/src/check/orca_worker.t.h
+++ gromosXX/src/check/orca_worker.t.h
@@ -7,6 +7,8 @@
 
 #include "qm_worker.t.h"
 
+namespace test {
+
 class Orca_Worker_Test : public QM_Worker_Test {
 
 public:
@@ -15,15 +17,25 @@ public:
    * Construct a new qm worker test object
    * 
    */
-  Orca_Worker_Test();
+  Orca_Worker_Test(std::string binary_name
+               , std::string test_title
+               , std::string topology_file
+               , std::string simulation_file
+               , std::string configuration_file
+               , std::string qmmm_file
+               , std::string coordinate_trajectory_file
+               , std::string energy_trajectory_file
+               , std::string final_configuration_file);
 
 
   /**
    * Destroy the qm worker test object
    * 
    */
-  virtual ~Orca_Worker_Test();
+  virtual ~Orca_Worker_Test() = default;
 
 };
+
+}
 
 #endif	/* ORCA_WORKER_T_H */
