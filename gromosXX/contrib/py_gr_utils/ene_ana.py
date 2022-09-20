44ded10f2d41945612eef9a63dc193c1fbbf985b
Drazen Petrov <drazen.petrov@boku.ac.at>
2022-09-20 14:24:46 +0000

    simulation tests & debug compilation

---
+++ gromosXX/contrib/py_gr_utils/ene_ana.py
@@ -0,0 +1,139 @@
+import numpy as np
+
+class EnegyTrajectory():
+    def __init__(self, lib_path, ene_trj_path=None, fene_trj_path=None):
+        """
+        A class that reads and stores all informations form the tre.
+        """
+        
+        # initialize some vars
+        self._data_type = None
+        self._data_type_dict = dict()
+        self._tre = list()
+
+        self.lib_path = lib_path
+        self.read_ene_ana_lib()
+        self.block_names = list(self.param_dict)
+        if ene_trj_path:
+            self.trj_path = ene_trj_path
+            self.read_trj()
+        elif fene_trj_path:
+            self.trj_path = fene_trj_path
+            self.trf = self.read_trj()
+
+    def read_ene_ana_lib(self):  
+        read_flag = 0
+        self.param_dict = {}
+        self.size_dict = {} 
+        with open(self.lib_path, 'r') as inp:
+            for line in inp:
+                if line.startswith('#'):
+                    continue
+                elif line.startswith('ENERTRJ'):
+                    read_flag = 1
+                elif line.startswith('FRENERTRJ'):
+                    read_flag = 2
+                if line.startswith('END'):
+                    read_flag = 0
+                elif read_flag == 1:
+                    parts = line.split()
+                    if parts[0] == 'block':
+                         self.param_dict[parts[1]] = {}
+                         self.size_dict[parts[1]] = {}
+                         block = parts[1]
+                    if parts[0] == 'subblock':    
+                        self.param_dict[block][parts[1]] = []
+                        if parts[2].isnumeric():
+                            self.param_dict[block][parts[1]].append(int(parts[2]))
+                        else:
+                            self.param_dict[block][parts[1]].append(str(parts[2]))
+                        if parts[3].isnumeric():
+                            self.param_dict[block][parts[1]].append(int(parts[3]))
+                        else:
+                            self.param_dict[block][parts[1]].append(str(parts[3]))
+                    elif parts[0] == 'size':
+                        self.size_dict[block][parts[1]] = None
+                        self.param_dict[block][parts[1]] = None
+
+    def __get_values_block(self, block_name, value_list):
+        data_all = list()
+        shapes_all = list()
+        pos = 0
+        for subblock, temp_shape in self.param_dict[block_name].items():
+            if temp_shape:
+                for i, temp_value in enumerate(temp_shape):
+                    if isinstance(temp_value, str):
+                        temp_shape[i] = self.size_dict[block_name][temp_value]
+                temp_shape = tuple(temp_shape)
+                num_values = 1
+                for temp_value in temp_shape:
+                    num_values *= temp_value
+                new_pos = pos + num_values
+                temp_data = np.array(value_list[pos:new_pos]).reshape(temp_shape)
+                data_all.append(temp_data)
+                if self._data_type is None:
+                    shapes_all.append((subblock, float, temp_shape))
+                pos = new_pos
+            else:
+                self.size_dict[block_name][subblock] = int(value_list[pos])
+                if subblock=="NUM_ENERGY_GROUPS":
+                    temp_value = self.size_dict[block_name][subblock]
+                    self.size_dict[block_name]["matrix_NUM_ENERGY_GROUPS"] = temp_value * (temp_value+1) // 2
+                pos += 1
+        if self._data_type is None:
+            self._data_type_dict[block_name] = np.dtype(shapes_all)
+        return data_all
+
+    def __create_dtype(self):
+        subblock_data_types = list()
+        for block_name, temp_data_type in self._data_type_dict.items():
+            subblock_data_types.append((block_name, temp_data_type, (1,)))
+        self._data_type = np.dtype(subblock_data_types)
+
+    def __store_step_data(self, data_step):
+        if self._data_type is None:
+            self.__create_dtype()
+        formated_data = list()
+        for i, temp_data in enumerate(data_step):
+            temp_data = np.array(tuple(temp_data), dtype=self._data_type[i])
+            formated_data.append(temp_data)
+        temp_step_data = np.array(tuple(formated_data), dtype=self._data_type)
+        self._tre.append(temp_step_data)
+
+    def read_trj(self):
+        data_step = None
+        flag = False
+        value_list = list()
+        with open(self.trj_path, 'r') as inp:
+            for line in inp:
+                if line.startswith("END"):
+                    if flag:
+                        temp_data = self.__get_values_block(flag, value_list)
+                        data_step.append(temp_data)
+                    flag=False
+                line = line.strip()
+                pos = line.find("#")
+                if pos!=-1:
+                    line = line[:pos]
+                if line and flag:
+                    value_list += line.split()
+                elif line in self.block_names:
+                    flag = line
+                    value_list = list()
+                    if flag=="TIMESTEP":
+                        if data_step:
+                            self.__store_step_data(data_step)
+                        data_step = list()
+                    continue
+        if data_step:
+            #print(data_step)
+            self.__store_step_data(data_step)
+        self.tre = np.array(self._tre, dtype=self._data_type)
+
+if __name__ == "__main__":
+    tr = Ene_Ana('ene_ana.md++.lib',ene_trj_path='md_peptide_1.tre')
+    with open("md_peptide_1.tre.npy", "wb") as f:
+        np.save(f, tr._tre)
+    with open("md_peptide_1.tre.npy", "rb") as f:
+        tre = np.load(f)
+    
---
+++ gromosXX/contrib/py_gr_utils/tests/_config_test_params.py
@@ -0,0 +1,219 @@
+import os
+import stat
+import shutil
+import yaml
+import numpy as np
+
+# default paths for GITLAB
+_MD_BIN = '${CI_PROJECT_DIR}/${BIN_PATH}'
+_CI_PROJECT_DIR = os.getenv('CI_PROJECT_DIR')
+_ENE_ANA_LIB = f'{_CI_PROJECT_DIR}/gromosXX/data/ene_ana.md++.lib'
+
+_run_file_header = """#!/bin/sh
+
+# first we set some variables
+NAME=`whoami`
+PROGRAM={md_bin}
+#output log file
+OUNIT={omd}
+
+"""
+
+_md_header = """MDOK=1
+
+${{PROGRAM}} \\
+@topo        {TOPO} \\
+@conf        {INPUTCRD} \\
+@input       {IUNIT} \\
+"""
+
+_run_file_tail = """>            ${OUNIT}
+grep "finished successfully" ${OUNIT} > /dev/null || MDOK=0
+
+uname -a >> ${OUNIT}"""
+
+class ConfigTest():
+    def __init__(self, config_path: str) -> None:
+        """
+        Creates executable run file from default values.
+        -----
+        input:
+                config_path: str --> path to yaml file
+        output:
+                md.run: file --> gromos run file
+        -----
+        """
+        self.expected_values = dict()
+        self.config_path = config_path
+        self.config_path_dir = os.path.dirname(self.config_path)
+        self.load_config_yaml()
+        self.get_run_string()
+        #self.load_expected_values()
+        #self.dump_full_config_yaml() #enable for local testing
+        #self.run_string()
+        #self.write_run_file()
+
+    def __get_input_file_path(self, file_path):
+        if os.path.exists(file_path):
+            return file_path, os.path.abspath(file_path)
+        else:
+            file_path = os.path.join(self.input_dir, file_path)
+            return file_path, os.path.abspath(file_path)
+
+    def load_config_yaml(self) -> dict:
+        """
+        Loads content of yaml file into a python dict.
+        -----
+        input:
+                self.config_path: str --> path to yaml file
+        output:
+                settings_loaded: dict --> params need for run file
+                                          in a nested dict.
+        -----
+        """
+        with open(self.config_path, "r") as stream:
+            try:
+                # load the yaml file
+                settings_loaded = yaml.safe_load(stream)
+                self.config = settings_loaded
+                # input dir
+                if self.config['input']['input_dir']:
+                    self.input_dir_abs = os.path.abspath(self.config['input']['input_dir'])
+                    self.input_dir_rel = self.config['input']['input_dir']
+                else:
+                    self.input_dir_abs = os.path.abspath(self.config_path_dir)
+                    self.input_dir_rel = self.config_path_dir
+                self.input_dir = self.input_dir_rel
+                # simulation dir
+                if self.config['input']['simulation_dir']:
+                    self.input_dir = self.input_dir_abs
+                    self.sim_dir = self.config['input']['simulation_dir']
+                    if os.path.abspath(self.sim_dir) != self.sim_dir:
+                        self.sim_dir = os.path.join(self.input_dir, self.sim_dir)
+                else:
+                    self.sim_dir = self.config_path_dir
+                # ene ana lib
+                if self.config['input']['ene_ana_lib']:
+                    ene_ana_lib = self.__get_input_file_path(self.config['input']['ene_ana_lib'])
+                    self.ene_ana_lib = ene_ana_lib[1]
+                else:
+                    self.ene_ana_lib = _ENE_ANA_LIB
+                # get the binary
+                if self.config['input']['md_binary']:
+                    self.md_bin = self.config['input']['md_binary']
+                else:
+                    self.md_bin = _MD_BIN
+                _pre_comm = os.getenv('PRE_MD_BIN')
+                if _pre_comm: # pre_command (e.g. mpirun)
+                    self.md_bin = '"${PRE_MD_BIN} ' + self.md_bin + '"'
+            except yaml.YAMLError as exc:
+                print(exc)
+            return settings_loaded
+
+    def dump_full_config_yaml(self):
+        """
+        Dumps content of dict into yaml file.
+        """
+        with open(f"{self.output}/used_inp.yaml", 'w') as file:
+            yaml.dump(self.config, file)
+
+    def get_run_string(self):
+        """
+        Inserts params from settings dict into run file string.
+        -----
+        input:
+                self.md_bin: str --> path to md binary. Default path to md in docker image.
+                self.sim_dir: str --> path of simulation dir. Default current dir.
+                self.input_dir: str --> path to input dir containing input files.
+                self.topo: str --> name of topology f.ile
+                self.iunit: str --> name of imd file.
+                self.icrd: str --> name of input cnf file.
+                self.omd: str --> name for gromos output file.
+                self.ocrd: str --> name of output cnf file.
+                self.trc: str --> name of coordinate trajectory.
+                self.tre: str --> name of energy trajectory.
+                self.trf: str --> name of force trajectory.
+                self.trv: str --> name of velocity trajectory.
+        output:
+                self.run_str: str --> formated string
+        """
+        _md_line_format = '@{:<12}{:} \\\n'
+        out_files_dict = dict(OUTPUTCRD='fin', OUTPUTTRX='trc', OUTPUTTRE='tre', 
+                              OUTPUTTRG='trg', OUTPUTTRF='trf', OUTPUTTRV='trv')
+        in_files_dict = dict() # pos res for instance, not implemented yet...
+        # get the run file
+        # header
+        self.omd = self.config['output']['OUNIT'] # out log file (omd)
+        self.run_str = _run_file_header.format(md_bin=self.md_bin, sim_dir=self.sim_dir, omd=self.omd)
+        # input files
+        self.topo = os.path.join(self.input_dir, self.config['input']['TOPO'])
+        self.iunit = os.path.join(self.input_dir, self.config['input']['IUNIT'])
+        self.icrd = os.path.join(self.input_dir, self.config['input']['INPUTCRD'])
+        self.run_str += _md_header.format(TOPO=self.topo, IUNIT=self.iunit, INPUTCRD=self.icrd)
+        for var_name, flag in in_files_dict.items():
+            if var_name in self.config['input'] and self.config['input'][var_name]:
+                temp_file_name = os.path.join(self.input_dir, self.config['input'][var_name])
+                self.run_str += _md_line_format.format(flag, temp_file_name)
+        # output files
+        self.out_files = {}
+        for var_name, flag in out_files_dict.items():
+            if var_name in self.config['output'] and self.config['output'][var_name]:
+                temp_file = self.config['output'][var_name]
+                self.run_str += _md_line_format.format(flag, temp_file)
+                self.out_files[flag] = temp_file
+        self.run_str += _run_file_tail
+
+    def write_run_file(self, run_file="md.run", run_str=None, sim_dir=None):
+        """
+        Writes out run_string into a executable run file.
+        -----
+        input:
+                run_file: str --> path to a file
+                run_str: str --> formated string containing run file structure (taken from self.run)
+                sim_dir: str --> simulation directory (also where run_file is made; taken from self.sim_dir)
+        output:
+                run_file: str  --> path to the gromos run file.
+        -----
+        """
+        if sim_dir is None:
+            sim_dir = self.sim_dir
+        if sim_dir:
+            if not os.path.isdir(sim_dir):
+                os.mkdir(sim_dir)
+            run_file = os.path.join(sim_dir, run_file)
+        if run_str is None:
+            run_str = self.run_str
+        with open(run_file, 'w+') as f:
+            f.write(run_str)
+        st = os.stat(run_file)
+        os.chmod(run_file, st.st_mode | stat.S_IEXEC)
+        return os.path.abspath(run_file)
+
+    def __store_expected_values(self, trj_type, temp_file):
+        temp_file = self.__get_input_file_path(temp_file)
+        with open(temp_file[1], "rb") as f:
+            self.expected_values[trj_type] = np.load(f)
+        return
+
+    def load_expected_values(self, *args):
+        """
+        loads hardcoded expected values (based on the self.conif file paths)
+        -----
+        input:
+                *args: list of file types (e.g. 'tre', 'trg')
+        output:
+                None (stores the values in self.expected_values)
+        -----
+        """
+        if args:
+            for trj_type in args:
+                temp_file = self.config['expected_values'][trj_type]
+                self.__store_expected_values(trj_type, temp_file)
+        else:
+            for trj_type, temp_file in self.config['expected_values'].items():
+                self.__store_expected_values(trj_type, temp_file)
+
+    def clean_up(self, flag_rm_input_dir=False):
+        if os.path.abspath(self.sim_dir) != self.input_dir_abs or flag_rm_input_dir:
+            if os.path.isdir(self.sim_dir):
+                shutil.rmtree(self.sim_dir)
