7a5516a491aab2d77495224ba870e790680b18dc
Peter Poliak <peter.poliak@boku.ac.at>
2023-08-31 14:40:12 +0200

    Force termination on boundary check error

--- gromosXX/src/interaction/qmmm/qmmm_interaction.cc
+++ gromosXX/src/interaction/qmmm/qmmm_interaction.cc
@@ -44,12 +44,12 @@ interaction::QMMM_Interaction * interaction::QMMM_Interaction::qmmm_ptr = nullpt
 
 interaction::QMMM_Interaction::QMMM_Interaction() : Interaction("QMMM Interaction")
                                                   , m_parameter()
-                                                  , m_set_size(1)
-                                                  , m_rank(0)
-                                                  , m_size(1)
                                                   , m_worker(nullptr)
                                                   , m_qm_zone(nullptr)
                                                   , m_qm_buffer(nullptr)
+                                                  , m_set_size(1)
+                                                  , m_rank(0)
+                                                  , m_size(1)
   {
 #ifdef XXMPI
     m_rank = MPI::COMM_WORLD.Get_rank();
