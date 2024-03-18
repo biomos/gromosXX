ea3d28730d17d2d6af752494687d4024472891f3
Maria Pechlaner <maria.pechlaner@chem.ethz.ch>
2024-03-18 14:04:42 +0100

    added license and updated doxygen documentation

--- md++/src/io/topology/in_zaxisoribias.cc
+++ md++/src/io/topology/in_zaxisoribias.cc
@@ -1,3 +1,23 @@
+/*
+ * This file is part of GROMOS.
+ * 
+ * Copyright (c) 2011, 2012, 2016, 2018, 2021, 2023 Biomos b.v.
+ * See <https://www.gromos.net> for details.
+ * 
+ * GROMOS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 /**
  * @file in_zaxisoribias.cc
  * implements methods of In_Topology.
@@ -70,34 +90,11 @@ io::In_Zaxisoribias::read(topology::Topology& topo,
 }
 
 /**
- * @section distanceresspec DISTANCERESSPEC block
- * The DISTANCERESSPEC block is read from the distance restraints specification
- * file.
+ * @section zaxisoribiasspec z-axis orientation bias block
+ * The ZAXISORIBIASSPEC block is read from the z-axis orientation bias
+ * restraints specification file.
  *
- * \c DISH is the carbon-hydrogen, \c DISC the carbon-carbon distance.
- * See @ref util::virtual_type for valid virtual atom types.
- * \c r0 is the restraint distance, \c w0 a weight factor (multiplied by the force
- * constant specified in the input file, \c CDIR) and rah specifies the type of
- * restraint (half harmonic repulsive, full harmonic, half harmonic attractive).
- * The restraint may be applied in a reduced set of dimensions, which is also set
- * by the value of \c rah. Allowed values of rah are \c dim - 1, \c dim or
- * \c dim + 1, where dim can take the following values:
- * - dim = 0  : dimensions to apply distance restraint: X, Y, Z
- * - dim = 10 : dimensions to apply distance restraint: X, Y
- * - dim = 20 : dimensions to apply distance restraint: X, Z
- * - dim = 30 : dimensions to apply distance restraint: Y, Z
- * - dim = 40 : dimension to apply distance restraint: X
- * - dim = 50 : dimension to apply distance restraint: Y
- * - dim = 60 : dimension to apply distance restraint: Z
- *
- * The type of restraint is determined as follows:
- * - rah = dim - 1: half harmonic repulsive distance restraint
- * - rah = dim: full harmonic distance restraint
- * - rah = dim + 1: half harmonic attractive distance restraint
- *
- * @todo add documentation for further rah values for restraining only x,y,z or combinations
- *
- * @snippet snippets/snippets.cc DISTANCERESSPEC
+ * @snippet snippets/snippets.cc ZAXISORIBIASSPEC
  * @sa util::virtual_type util::Virtual_Atom
  */
 void io::In_Zaxisoribias::read_ZAXISORIBIASSPEC(topology::Topology &topo,
