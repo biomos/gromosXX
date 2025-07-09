e99048b6a9e9edcaf09d7490ed932c13c61a57b1
Peter Poliak <peter.poliak@boku.ac.at>
2025-07-09 17:20:23 +0200

    Improved CUDA managed memory allocator

--- md++/src/gpu/cuda/memory/cumallocator.h
+++ md++/src/gpu/cuda/memory/cumallocator.h
@@ -4,10 +4,8 @@
  * allocator for CUDA managed unified memory, CUDA transparent version of std::vector
  */
 
-#ifndef INCLUDED_CUMALLOCATOR_H
-#define INCLUDED_CUMALLOCATOR_H
+#pragma once
 
-#include <cuda_runtime.h>
 #include <iostream>
 #include <limits>
 #include <memory>
@@ -15,78 +13,79 @@
 #include <type_traits>
 #include <vector>
 
+#include "gpu/cuda/cuheader.h"
+
 namespace gpu {
 
-template <typename T>
-class CuMallocator {
-public:
-    using value_type = T;
-    using pointer = T*;
-    using const_pointer = const T*;
-    using size_type = std::size_t;
-    using difference_type = std::ptrdiff_t;
+    template <typename T>
+    class CuMallocator {
+    public:
+        using value_type = T;
+        using pointer = T*;
+        using const_pointer = const T*;
+        using size_type = std::size_t;
+        using difference_type = std::ptrdiff_t;
+        using allocator_type = CuMallocator<T>;
 
-    using propagate_on_container_copy_assignment = std::true_type;
-    using propagate_on_container_move_assignment = std::true_type;
-    using propagate_on_container_swap = std::true_type;
-    using is_always_equal = std::true_type;
+        using propagate_on_container_copy_assignment = std::true_type;
+        using propagate_on_container_move_assignment = std::true_type;
+        using propagate_on_container_swap = std::true_type;
+        using is_always_equal = std::true_type;
 
-    template <typename U>
-    struct rebind { using other = CuMallocator<U>; };
+        template <typename U>
+        struct rebind { using other = CuMallocator<U>; };
 
-    CuMallocator() noexcept = default;
-    template <typename U> constexpr CuMallocator(const CuMallocator<U>&) noexcept {}
+        CuMallocator() noexcept = default;
+        template <typename U> constexpr CuMallocator(const CuMallocator<U>&) noexcept {}
 
-    T* allocate(std::size_t n) {
-        if (n > std::numeric_limits<size_type>::max() / sizeof(T))
-            throw std::bad_array_new_length();
+        T* allocate(std::size_t n) {
+            if (n > std::numeric_limits<size_type>::max() / sizeof(T))
+                throw std::bad_array_new_length();
 
-        T* p = nullptr;
-        cudaError_t err = cudaMallocManaged(&p, n * sizeof(T));
-        if (err != cudaSuccess) {
-            std::cerr << "cudaMallocManaged failed: " << cudaGetErrorString(err) << '\n';
-            throw std::bad_alloc();
+            T* p = nullptr;
+            cudaError_t err = cudaMallocManaged(&p, n * sizeof(T));
+            if (err != cudaSuccess) {
+                std::cerr << "cudaMallocManaged failed: " << cudaGetErrorString(err) << '\n';
+                throw std::bad_alloc();
+            }
+
+            report(p, n, true);
+            return p;
         }
 
-        report(p, n, true);
-        return p;
-    }
+        void deallocate(T* p, std::size_t n) noexcept {
+            report(p, n, false);
+            cudaFree(p);
+        }
 
-    void deallocate(T* p, std::size_t n) noexcept {
-        report(p, n, false);
-        cudaFree(p);
-    }
+#if !defined(__CUDA_ARCH__) && __cplusplus < 202002L
+        template <typename U, typename... Args>
+        void construct(U* p, Args&&... args) {
+            ::new((void*)p) U(std::forward<Args>(args)...);
+        }
 
-#ifndef __CUDA_ARCH__
-    template <typename U, typename... Args>
-    void construct(U* p, Args&&... args) {
-        ::new((void*)p) U(std::forward<Args>(args)...);
-    }
-
-    template <typename U>
-    void destroy(U* p) {
-        p->~U();
-    }
+        template <typename U>
+        void destroy(U* p) {
+            p->~U();
+        }
 #endif
 
-private:
-    void report(T* p, std::size_t n, bool alloc) const {
+    private:
+            void report(T* p, std::size_t n, bool alloc) const {
 #ifndef NDEBUG
-        std::cout << (alloc ? "Alloc: " : "Dealloc: ")
-                  << n * sizeof(T) << " bytes at " << static_cast<void*>(p) << '\n';
+                std::cout << (alloc ? "Alloc: " : "Dealloc: ")
+                        << n * sizeof(T) << " bytes at " << static_cast<void*>(p) << '\n';
 #endif
-    }
-};
+            }
+        };
 
-template <typename T, typename U>
-bool operator==(const CuMallocator<T>&, const CuMallocator<U>&) { return true; }
+        template <typename T, typename U>
+        bool operator==(const CuMallocator<T>&, const CuMallocator<U>&) { return true; }
 
-template <typename T, typename U>
-bool operator!=(const CuMallocator<T>&, const CuMallocator<U>&) { return false; }
+        template <typename T, typename U>
+        bool operator!=(const CuMallocator<T>&, const CuMallocator<U>&) { return false; }
 
-template <typename T>
-using cuvector = std::vector<T, CuMallocator<T>>;
+        template <typename T>
+        using cuvector = std::vector<T, CuMallocator<T>>;
 
 } // namespace gpu
-
-#endif
