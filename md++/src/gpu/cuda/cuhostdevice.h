705e9feff7f63f7641fdcc35b0eced4ac053d3a3
Peter Poliak <peter.poliak@boku.ac.at>
2025-08-29 14:30:35 +0200

    HOSTDEVICE macro

---
+++ md++/src/gpu/cuda/cuhostdevice.h
@@ -0,0 +1,32 @@
+/*
+ * This file is part of GROMOS.
+ * 
+ * Copyright (c) 2011, 2012, 2016, 2018, 2021, 2023 Biomos b.v.
+ * See <https://www.gromos.net> for details.
+ * 
+ * GROMOS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file cuhostdevice.h
+ * definition of HOSTDEVICE to allow shared code between host and device
+ */
+
+#pragma once
+
+#ifdef __CUDACC__
+    #define HOSTDEVICE __host__ __device__ __forceinline__
+#else
+    #define HOSTDEVICE inline
+#endif
